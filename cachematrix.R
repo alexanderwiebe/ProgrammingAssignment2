6e1161967afbd47753beb12a64c69796214d90a2
Alexander Wiebe <Alexander.Wiebe@solvera.ca>
2016-02-27 15:30:42 -0600

    Init commit finish assignment 2
    created the two functions for calculating the correct values.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: 2
+## Author: Alexander Wiebe
+## Date: Feb 27, 2016
 
-## Write a short comment describing this function
+## The following two functions deal with caching the inverse of a matrix
 
+## makeCacheMatrix creates an object and once the inverse of the matrix is 
+## calculated, it caches the result
 makeCacheMatrix <- function(x = matrix()) {
-
+   myInv <- NULL
+   set <- function(y) {
+      x <<- y
+      myInv <<- NULL
+   }
+   get <- function() x
+   setInverse <- function(solve) myInv <<- solve
+   getInverse <- function() myInv
+   list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve calculates the inverse of the matrix object from the above 
+## function.  If the matrix object already has calculated the inverse 
+## cacheSolve will use the cached version.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## Return a matrix that is the inverse of 'x'
+   myInv <- x$getInverse()
+   if(!is.null(myInv)) {
+      message("getting cached data")
+      return(myInv)
+   }
+   data <- x$get()
+   myInv <- solve(data, ...)
+   x$setInverse(myInv)
+   myInv
 }
